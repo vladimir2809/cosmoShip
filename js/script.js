cf535e3e612e269114f9a3e059fa7d7bdee2a186
vladimir2809 <46346862+vladimir2809@users.noreply.github.com>
2022-01-07 10:38:46 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 var context;
 var screenWidth=800;
 var screenHeight=600;
-var mapWidth=2400;
-var mapHeight=2400;
+var mapWidth=2800;
+var mapHeight=2800;
 var quantityMeteor=150;
 var quantityBarrel=20;
 var countLoadImage=0;
+var energy=100;
+var maxEnergy=energy;
 var nameImageArr=['spaceship','meteor',"barrel","background"];
 var imageArr=new Map();// массив картинок
 map ={
@@ -42,8 +44,8 @@ var barrelType={
     being:false,
     x:0,
     y:0,
-    width:26,
-    height:30,
+    width:35,
+    height:35,
 }
 noMoveObjArr=[];
 closureMap={
@@ -97,6 +99,8 @@ window.addEventListener('load', function () {
 });
 function preload()// функция предзагрузки
 {
+    let timeNow=new Date().getTime()
+    srand(timeNow);
     loadImageArr();
 }
 function create ()// функция создание обьектов неоюходимых для игры
@@ -112,16 +116,18 @@ function create ()// функция создание обьектов неоюх
     initMeteors();
     initBarrel();
     
-    for (let i=0;i<meteorArr.length;i++)
-    {
-        let obj=clone(meteorArr[i]);
-        noMoveObjArr.push(obj)
-    }
-     for (let i=0;i<barrelArr.length;i++)
-    {
-        let obj=clone(barrelArr[i]);
-        noMoveObjArr.push(obj)
-    }
+//    
+//    for (let i=0;i<meteorArr.length;i++)
+//    {
+//        let obj=clone(meteorArr[i]);
+//        noMoveObjArr.push(obj)
+//    }
+//     for (let i=0;i<barrelArr.length;i++)
+//    {
+//        let obj=clone(barrelArr[i]);
+//        noMoveObjArr.push(obj)
+//    }
+//    
     let obj;
     for (let i=0;i<noMoveObjArr.length;i++)
     {
@@ -149,7 +155,7 @@ function create ()// функция создание обьектов неоюх
         }
    
     }
-    console.log(closureMap);
+    //console.log(closureMap);
 }
 function drawBackground()
 {
@@ -173,19 +179,43 @@ function drawAll()
     context.fillStyle='rgb(210,210,210,1)';
     //context.clearRect(0,0,camera.width,camera.height);// очистка экрана
     context.drawImage(imageArr.get("background"),1,1);
-    drawTurnSprite(context,imageArr.get("spaceship"),
-            screenWidth/2,screenHeight/2,ship.angle,15,10,{x:1,y:1},1);
+   
     for (let i=0;i<meteorArr.length;i++)
     {
-        drawSprite(context,imageArr.get("meteor"),
+        if (meteorArr[i].being==true)
+        {
+            drawSprite(context,imageArr.get("meteor"),
                 meteorArr[i].x,meteorArr[i].y,camera,1) 
+        }
     }
     for (let i=0;i<barrelArr.length;i++)
     {
-        drawSprite(context,imageArr.get("barrel"),
-                barrelArr[i].x,barrelArr[i].y,camera,1) 
+        if (barrelArr[i].being==true)
+        {
+            drawSprite(context,imageArr.get("barrel"),
+                    barrelArr[i].x,barrelArr[i].y,camera,1) 
+        }
     }     
-    //рисуем обьекты для замыкания
+    drawClosureObj();
+    drawEnergy();
+    drawTurnSprite(context,imageArr.get("spaceship"),
+            screenWidth/2,screenHeight/2,ship.angle,15,10,{x:1,y:1},1); 
+
+}
+function drawEnergy()
+{
+    let x=20, y=20,width=40, height=15;
+    context.strokeStyle="#FFFFFF";
+    context.fillStyle='#FFFFFF';
+    context.strokeRect(x,y,width,height);
+    context.fillRect(x-5,y+5,5,5);
+    context.fillStyle='#0000FF';
+    if (energy<maxEnergy*0.25) context.fillStyle='#FF0000';
+    let valuePix=energy/maxEnergy*(width-2);
+    context.fillRect(x+1+(width-2)-valuePix,y+1,valuePix,height-2);
+}
+function drawClosureObj()
+{//рисуем обьекты для замыкания
     for (let i=0;i<closureMap.down.length;i++)
     {
        
@@ -208,8 +238,6 @@ function drawAll()
         drawSprite(context,imageArr.get(closureMap.left[i].id),
             closureMap.left[i].x+map.width,closureMap.left[i].y,camera,1); 
     }
-        
-
 }
 function drawSprite(context,image,x,y,camera,scale)// функция вывода спрайта на экран
 {
@@ -247,11 +275,27 @@ function gameLoop()
     
     if (checkPressKey( "ArrowUp"))
     {
-        if (ship.speed<10)ship.speed+=acceleration;else ship.speed=maxSpeed;    
+        if (ship.speed<10)
+        {
+            ship.speed+=acceleration;
+            energy-=0.2;
+        }
+        else
+        {
+            ship.speed=maxSpeed;
+        }    
     } 
     else  if (checkPressKey( "ArrowDown"))
     {
-        if (ship.speed>0)ship.speed-=acceleration;else ship.speed=0;
+        if (ship.speed>0)
+        {
+            ship.speed-=acceleration;
+            energy-=0.2;
+        }
+        else
+        {
+            ship.speed=0;
+        }
        // alert(5415);
     }else 
     {
@@ -259,12 +303,33 @@ function gameLoop()
     }
     ship.y-=Math.sin(pi*(ship.angle ) / 180)*ship.speed;
     ship.x-=Math.cos(pi*(ship.angle ) / 180)*ship.speed;
+    for (let i=0;i<barrelArr.length;i++)
+    {
+        if (barrelArr[i].being==true)
+        if (ship.x+ship.width>barrelArr[i].x && 
+            ship.x<barrelArr[i].x+barrelArr[i].width &&
+            ship.y+ship.height>barrelArr[i].y && 
+            ship.y<barrelArr[i].y+barrelArr[i].height 
+           )
+        {
+            if (energy<maxEnergy*0.8) 
+            {
+                energy+=maxEnergy*0.2;
+            }
+            else
+            {
+                energy=maxEnergy;
+            }
+            barrelArr[i].being=false;
+        }
+    }
     //условия для замыкания
     if (ship.y<map.y) ship.y=map.height;
     if (ship.y>map.height) ship.y=map.y;
     if (ship.x<map.x) ship.x=map.width;
     if (ship.x>map.width) ship.x=map.x;
-    
+    if (energy>0) energy-=0.05;// else energy=maxEnergy
+    if (energy<=0) alert('У вас закончилась энергия, игра окончена.');
     camera.x=ship.x-screenWidth/2;
     camera.y=ship.y-screenHeight/2;
 
@@ -274,10 +339,15 @@ function initMeteors()
     for (let i=0;i<quantityMeteor;i++)
     {
         let meteorOne=clone(meteorType);
-        meteorOne.x=randomInteger(1,mapWidth);
-        meteorOne.y=randomInteger(1,mapHeight);
+        //meteorOne.x=randomInteger(1,mapWidth);
+        //meteorOne.y=randomInteger(1,mapHeight);
+        //let meteorOne=clone(meteorType);
+        let XY=calcNoColiisionXY(meteorOne);
+        meteorOne.x=XY.x;
+        meteorOne.y=XY.y;
         meteorOne.being=true;
         meteorArr.push(meteorOne);
+        noMoveObjArr.push(meteorOne);
     }
 }
 function initBarrel()
@@ -285,10 +355,45 @@ function initBarrel()
     for (let i=0;i<quantityBarrel;i++)
     {
         let barrelOne=clone(barrelType);
-        barrelOne.x=randomInteger(1,mapWidth);
-        barrelOne.y=randomInteger(1,mapHeight);
+        let XY=calcNoColiisionXY(barrelOne);
+        barrelOne.x=XY.x;
+        barrelOne.y=XY.y;
         barrelOne.being=true;
         barrelArr.push(barrelOne);
+        noMoveObjArr.push(barrelOne);
     }
+}
+function calcNoColiisionXY(obj)
+{
+    let flag=false;
+    let x=null,y=null;
+    do
+    {
+        flag=false;
+        x=randomInteger(1,mapWidth);
+        y=randomInteger(1,mapHeight);
+        if (checkCollisionNoMoveObj({x:x,y:y,width:obj.width,height:obj.height})==true)
+        {
+            flag=true;
+        }
+    }while(flag==true);
+    return {x:x,y:y};
+}
+function checkCollisionNoMoveObj(obj)
+{
+    
+    for (let i=0;i<noMoveObjArr.length;i++)
+    {
+        if (obj.x+obj.width>noMoveObjArr[i].x && 
+            obj.x<noMoveObjArr[i].x+noMoveObjArr[i].width &&
+            obj.y+obj.height>noMoveObjArr[i].y && 
+            obj.y<noMoveObjArr[i].y+noMoveObjArr[i].height
+            )
+        {
+            return true;
+            
+        }
+    }
+    return false;
 }
 
